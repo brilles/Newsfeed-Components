3046c66eb77ac895d539d2295ca8f0ba64ed24e9
Brian Illes <brianrilles@gmail.com>
2019-01-30 15:48:39 -0800

    stretch: added native CSS animations, img carousel with GSAP and jQuery

--- Article/Article.js
+++ Article/Article.js
@@ -4,17 +4,17 @@ class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
     this.domElement = domElement;
-    // create a reference to the ".expandButton" class. 
-    this.expandButton = domElement.querySelector('.expandButton');
+    // create a reference to the ".expandButton" class.
+    this.expandButton = domElement.querySelector(".expandButton");
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.textContent = "expand";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    this.expandButton.addEventListener('click', () => this.expandArticle());
+    this.expandButton.addEventListener("click", () => this.expandArticle());
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    this.domElement.classList.toggle('article-open')
+    this.domElement.classList.toggle("article-open");
   }
 }
 
@@ -26,5 +26,5 @@ class Article {
 
 */
 
-let articles = document.querySelectorAll('.article');
+let articles = document.querySelectorAll(".article");
 articles.forEach(article => new Article(article));
